b49b7ef6ed8cfe1deb8e4dbdc8e3bca0d983dcc6
Nishthaa Jain <nishthaa2003@gmail.com>
2024-08-04 19:23:59 +0530

    Frontend

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: 'https://todos-backend-eight.vercel.app',
+    baseURL: 'https://todo-app-backend-rouge.vercel.app/',
     // baseURL: 'http://localhost:4444',
     withCredentials: true
 });
